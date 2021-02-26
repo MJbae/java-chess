258b58de782f9cbbe7b6ce410e550db42ac9b1b8
MJbae <gently.gogo@gmail.com>
2021-02-26 11:58:42 +0900

    Refactor: Position 클래스 도입에 따라 기본 생성자 포함 변경

--- src/main/java/mj/chess/pieces/Piece.java
+++ src/main/java/mj/chess/pieces/Piece.java
@@ -1,27 +1,20 @@
 package mj.chess.pieces;
 
+import java.util.List;
+
 public class Piece {
 
     private final Color color;
     private final Type type;
     private final String representation;
+    private final Position position;
 
-    private Piece(Color color, Type type) {
+    private Piece(Color color, Type type, Position position) {
 
         this.color = color;
         this.type = type;
         this.representation = type.getRepresentation(color);
-
-        validateColor(this.color);
-        validateType(this.type);
-        validateRepresentation(this.representation);
-    }
-
-    private Piece(Type type) {
-
-        this.color = Color.WHITE;
-        this.type = type;
-        this.representation = type.getRepresentation(color);
+        this.position = position;
 
         validateColor(this.color);
         validateType(this.type);
@@ -40,12 +33,49 @@ public class Piece {
         return representation;
     }
 
-    public boolean isBlack() {
-        return this.color == Color.BLACK;
+    public Position getPosition() {
+        return position;
     }
 
     public boolean isWhite() {
-        return this.color == Color.WHITE;
+        return matchColor(Color.WHITE);
+    }
+
+    public boolean isBlack() {
+        return matchColor(Color.BLACK);
+    }
+
+    public boolean matchColorAndType(Color color, Type type) {
+        return matchColor(color) && matchType(type);
+    }
+
+    private boolean matchColor(Color color) {
+        return this.color == color;
+    }
+
+    private boolean matchType(Type type) {
+        return this.type == type;
+    }
+
+    public void addPiecesByColor(Color color, List<Piece> piecesByColor) {
+        if (matchColor(color)) {
+            piecesByColor.add(this);
+        }
+    }
+
+    public double getPoint(List<Piece> pieces) {
+        if (!matchType(Type.PAWN)) {
+            return this.type.getDefaultPoint();
+        }
+
+        List<Position> columnNeighbors = this.position.getColumnNeighbors();
+        for (Position position : columnNeighbors) {
+            if (pieces.contains(new Piece(this.color, this.type, position))) {
+                return this.type.getDefaultPoint() - 0.5;
+            }
+        }
+
+        return this.type.getDefaultPoint();
     }
 
     private void validateColor(Color color) {
@@ -93,6 +123,28 @@ public class Piece {
         if (!isValid) {
             throw new IllegalArgumentException("Invalid representation");
         }
+    }
+
+    public boolean sameAs(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+
+        Piece other = (Piece) obj;
+        if (color != other.color)
+            return false;
+        if (position == null) {
+            if (other.position != null) {
+                return false;
+            }
+        }
+        if (type != other.type) {
+            return false;
+        }
+        return true;
     }
 
     public static class PieceMaker {
